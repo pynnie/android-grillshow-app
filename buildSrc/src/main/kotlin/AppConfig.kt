ca7eb3f3c02e8e7d6e86ee7a5fddf37ac39a36a9
pynnie <sebastian-hecken@gmx.de>
2025-04-06 16:43:13 +0200

    update app version to 1.0.2 and target sdk to 35

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,8 +1,8 @@
 object AppConfig {
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val minSdk = 29
-    const val targetSdk = 34
+    const val targetSdk = 35
 
-    const val versionName = "1.0"
-    const val versionCode = 10
+    const val versionName = "1.0.2"
+    const val versionCode = 102
 }
